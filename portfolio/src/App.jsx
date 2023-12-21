b4ae72e20f893f605adba60e5da55dbf90b71e2a
Dalius Beck <daliusbeckxiv@gmail.com>
2023-12-20 23:26:18 -0800

    Update App.jsx

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -6,10 +6,16 @@ function App() {
   return (
     <>
       <div>
+<<<<<<< Updated upstream
         <div className="home">
           <h1>this is the homepage</h1>
         </div>
       </div>
+=======
+        <h1>home page</h1>
+      </div>
+
+>>>>>>> Stashed changes
     </>
   )
 }
