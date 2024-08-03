43c21b4afd7643a5923106a0e62b88ef4ce69c64
Dalius Beck <daliusbeckxiv@gmail.com>
2024-08-03 00:05:20 -0700

    thinking of changing the contact me page into the footer to save room and time for people to connect with me

--- portfolio/src/pages/About.jsx
+++ portfolio/src/pages/About.jsx
@@ -4,10 +4,11 @@ const About = () => {
             <h3 className="title has-text-danger">
                 About Me 
             </h3>
-            <p className="is-size-5 mb-6">I am a creative Developer based in Arizona and I am passionate about creating stunning projects. I design and create static websites as well as creating API&apos;s for reactive web apps with the help of React and Python. I have always been fascinated with the backend part of the website, figuring out how it functions and trying to make my own websites fuctional using Python. Being a Virtual Creator and having a inconsiderable amount of ambition and optimism for creating amazing things is what drives me the most. 
-            
-            
-            {/* My expertise lies in building the backend for websites using Python as my primary programming language of choice. I enjoy making webistes and learning new challenging things that consist in figuring out problems to optimize a website to let if function better */} </p>
+            <p className="is-size-5 mb-6">I am a creative Developer based in California and I am passionate about creating stunning projects. I design and create static websites as well as creating API&apos;s for reactive web apps with the help of React and Python. I have always been fascinated with the backend part of the website, figuring out how it functions and trying to make my own websites fuctional using Python. Being a Virtual Creator and having a inconsiderable amount of ambition and optimism for creating amazing things is what drives me the most.
+            </p>
+            <p className="is-size-5">
+            In my free time. I enjoy going snowboarding, flying and traveling to other places, and even writing poetry when inspiration is afoot.
+            </p>
             {/* <div className="container">
                 <div className="columns is-centered mx-5">
                     <div className="column is-half card m-2">
